9389aae68321f6178ba31008e568ab771573b9b7
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-10-06 10:41:49 -0400

    few changes

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var app = express();
 
-app.get('/user/:name', function (req, res) {
-  res.send(req.params.name);
-});
+// app.get('/user/:name', function (req, res) {
+//   res.send(req.params.name);
+// });
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   console.log('hello');
-  //res.send('hello');
+  res.send('hello');
 });
 
 app.listen(5000, function () {
   console.log('listening to port 5000..');
 });
+
+//from beginning
+//const express = require('express')
+//const app = express();
